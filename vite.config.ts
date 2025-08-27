dacebea7afdc04a70f2f153adca14145fad316e3
Hamza Zafar <irexspare@gmail.com>
2025-08-27 16:57:37 +0500

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // <-- Required
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Fix for __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   base: '/RentEasyGo-Web',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // <-- Set alias
+      '@': resolve(__dirname, './src'),
     },
   },
 });
