9bc7835c0a5c2552dff35072ac5c40962a9ad9ea
Hamza Zafar <irexspare@gmail.com>
2025-08-27 17:06:53 +0500

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/RentEasyGo-Web',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // <-- Set alias
